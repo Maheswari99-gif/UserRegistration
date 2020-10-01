642907e38218cdb9db3bd5f018e8d2a1df19d0fb
Maheswari99-gif <maheswarikoyyani999@gmail.com>
2020-10-01 20:53:37 +0530

    usecase1

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,1 +1,16 @@
-User Registration Form
+import java.util.regex.*;
+import java.util.Scanner;
+public class UserRegistration{
+	public static void main(String[] args){
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter First Name");
+		String f=s.next();
+		String check="[A-Z]{1}[a-z]*";
+		Pattern p=Pattern.compile(check);
+		Matcher m=p.matcher(f);
+		if(m.matches())
+		System.out.println("Valid");
+		else
+		System.out.println("Invalid");
+	}
+}
