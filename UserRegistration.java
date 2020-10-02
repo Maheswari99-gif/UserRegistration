f8b86c81ab212309e993a1872445a7ef6ba9f3be
Maheswari99-gif <maheswarikoyyani999@gmail.com>
2020-10-02 13:49:52 +0530

    usecase3

--- UserRegistration.java
+++ UserRegistration.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 public class UserRegistration{
 	public static void main(String[] args){
 		Scanner s=new Scanner(System.in);
-		System.out.println("Enter Last Name");
-		String l=s.next();
-		String check="[A-Z]{1}[a-z]{3,}";
+		System.out.println("Enter Email Id");
+		String e=s.next();
+		String check="^[A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
 		Pattern p=Pattern.compile(check);
-		Matcher m=p.matcher(l);
+		Matcher m=p.matcher(e);
 		if(m.matches())
 		System.out.println("Valid");
 		else
