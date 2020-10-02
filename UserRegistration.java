6d95be03666ac7deebf70f35e57ca29101d06d9f
Maheswari99-gif <maheswarikoyyani999@gmail.com>
2020-10-02 21:10:32 +0530

    usecase9

--- UserRegistration.java
+++ UserRegistration.java
@@ -2,15 +2,15 @@ import java.util.regex.*;
 import java.util.Scanner;
 public class UserRegistration{
 	public static void main(String[] args){
-	try{
 		Scanner s=new Scanner(System.in);
-		System.out.println("Enter Your Password");
-		String p=s.next();
-		String check="(?=.*[A-Z])(?=.*[0-9])[*.!@#$%^&(){}[]:;<>,.?/~_+=-|]{1}[a-zA-Z0-9]{7,}$";
-		if(p.matches(check))
+		System.out.println("Enter Email Id");
+		String e=s.next();
+		String check="^[A-Za-z]+[A-Za-z0-9-,_]+.{0,1}+[a-zA-Z0-9]+@[A-Za-z0-9]+.[A-Za-z0-9,]+.{0,1}+[0-9]{0,1}+[A-Za-z,]{2,}+$";
+		Pattern p=Pattern.compile(check);
+		Matcher m=p.matcher(e);
+		if(m.matches())
 		System.out.println("Valid");
 		else
 		System.out.println("Invalid");
-		}catch(Exception e){return;}
 	}
 }
