18f40cb57182718832bbe69fc05de4076dc09049
Maheswari99-gif <maheswarikoyyani999@gmail.com>
2020-10-02 15:46:01 +0530

    usecase5

--- UserRegistration.java
+++ UserRegistration.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 public class UserRegistration{
 	public static void main(String[] args){
 		Scanner s=new Scanner(System.in);
-		System.out.println("Enter Mobile Number");
-		String no=s.next();
-		String check="[91]{2}\\s[6-9]{1}[0-9]{9}$";
-		if(no.matches(check))
+		System.out.println("Enter Your Password");
+		String p=s.next();
+		String check="[A-Za-z]{8,}$";
+		if(p.matches(check))
 		System.out.println("Valid");
 		else
 		System.out.println("Invalid");
